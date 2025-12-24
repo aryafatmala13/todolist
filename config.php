5425e0a96aa12b3e410234c6e28679494d916ba5
Wirapermana <wira.pendit@gmail.com>
2025-12-24 22:11:26 +0800

    Membuat config.php untuk konfigurasi koneksi database menggunakan PDO

---
+++ config.php
@@ -0,0 +1,22 @@
+<?php
+
+// Konfigurasi koneksi database
+$host = "localhost";
+$dbname = "todo_list";
+$user = "root";
+$pass = "";
+
+// Membuat koneksi PDO
+try {
+    $pdo = new PDO(
+        "mysql:host=$host;dbname=$dbname;charset=utf8",
+        $user,
+        $pass
+    );
+
+    // Mengatur mode error PDO
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+} catch (PDOException $e) {
+    die("Koneksi database gagal: " . $e->getMessage());
+}
