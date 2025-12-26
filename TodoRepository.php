0aac89eab61f677faf93b2d0cb1e56cf1cc4093c
krisna <krisnaputra0666@gmail.com>
2025-12-26 09:06:59 +0800

    penambahan to do repository

---
+++ TodoRepository.php
@@ -0,0 +1,36 @@
+<?php
+
+require_once __DIR__ . '/../config.php';
+require_once __DIR__ . '/Todo.php';
+
+// Repository untuk akses data todo
+class TodoRepository {
+
+    private $pdo;
+
+    public function __construct($pdo) {
+        $this->pdo = $pdo;
+    }
+
+    // Ambil semua data todo
+    public function getAll() {
+        $stmt = $this->pdo->query("SELECT * FROM todos ORDER BY id DESC");
+        return $stmt->fetchAll(PDO::FETCH_ASSOC);
+    }
+
+    // Simpan todo baru
+    public function create($title, $description) {
+        $stmt = $this->pdo->prepare(
+            "INSERT INTO todos (title, description) VALUES (?, ?)"
+        );
+        return $stmt->execute([$title, $description]);
+    }
+
+    // Hapus todo
+    public function delete($id) {
+        $stmt = $this->pdo->prepare(
+            "DELETE FROM todos WHERE id = ?"
+        );
+        return $stmt->execute([$id]);
+    }
+}
