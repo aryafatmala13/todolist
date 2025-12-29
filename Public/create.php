0281b84fe3fb74ba52753f00e3a7bb226bc23e11
AsdhikaMahardiasa <dikamahadiasa@gmail.com>
2025-12-29 23:03:32 +0800

    menambahkan validasi agar judul tidak kosong, menyimpan data todo ke database menggunakan TodoRepository

--- Public/create.php
+++ Public/create.php
@@ -5,3 +5,8 @@ require_once __DIR__ . '/../src/TodoRepository.php';
 
 $judul = trim($_POST['judul']);
 $deskripsi = $_POST['deskripsi'];
+
+if ($judul !== '') {
+    $repo = new TodoRepository($pdo);
+    $repo->create($judul, $deskripsi);
+}
