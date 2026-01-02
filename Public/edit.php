223632cde79e73e523ca0d0e1fa97980a2e94a6c
AsdhikaMahardiasa <dikamahadiasa@gmail.com>
2026-01-02 11:00:19 +0800

    mengambil data todo berdasarkan id dan validasi data

--- Public/edit.php
+++ Public/edit.php
@@ -2,3 +2,13 @@
 
 require_once __DIR__ . '/../config.php';
 require_once __DIR__ . '/../src/TodoRepository.php';
+
+$id = (int) $_GET['id'];
+
+$repo = new TodoRepository($pdo);
+$todo = $repo->getById($id);
+
+if (!$todo) {
+    die("Data tidak ditemukan");
+}
+?>
