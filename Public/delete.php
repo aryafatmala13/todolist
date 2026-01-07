61aca338a03aac2c57a07e85222ec781fb34cebe
Wirapermana <wira.pendit@gmail.com>
2026-01-07 08:58:17 +0800

    Menambah komentar pada baris kode delete.php

--- Public/delete.php
+++ Public/delete.php
@@ -1,12 +1,18 @@
 <?php
-
+// Koneksi database
 require_once __DIR__ . '/../config.php';
+// Repository Todo
 require_once __DIR__ . '/../src/TodoRepository.php';
 
+// Mengambil ID todo dari form (POST) dan memastikan tipe integer
 $id = (int) $_POST['id'];
 
+// Membuat object repository
 $repo = new TodoRepository($pdo);
+// Menghapus data todo berdasarkan ID
 $repo->delete($id);
 
+
+// Kembali ke halaman utama
 header("Location: index.php");
 exit;
